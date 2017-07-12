33ff4ad03b5b5b0609e1d16fc6f8d4fdc219f422
User <User@DESK-DGZ7J32.lexicon.edu.local>
2017-07-12 13:02:26 +0200

    fixed plane

--- MicroAirlines/src/Plane.java
+++ MicroAirlines/src/Plane.java
@@ -1,8 +1,25 @@
 
 public class Plane {
 	
-	public Plane() {
-		
+	private int economySeats;
+	private int firstclassSeats;
+	
+	
+	public int getEconomySeats() {
+		return economySeats;
+	}
+
+	public int getFirstclassSeats() {
+		return firstclassSeats;
+	}
+	
+	public int totalSeats() {
+		return economySeats+firstclassSeats;
+	}
+
+	public Plane(int numEconomySeats, int numFirstclassSeats) {	
+		this.economySeats=numEconomySeats;
+		this.firstclassSeats=numFirstclassSeats;
 	}
 
 }
