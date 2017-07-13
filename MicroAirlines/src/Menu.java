4b68981be34c5d47deaba11b18f0dd75d9ac7daa
User <User@DESK-9ZQD132.lexicon.edu.local>
2017-07-13 09:47:34 +0200

    Hallå!

--- MicroAirlines/src/Menu.java
+++ MicroAirlines/src/Menu.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-//
+
 public class Menu {
 
 	static Scanner in = new Scanner(System.in);
