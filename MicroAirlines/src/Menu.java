73bf352bd07c3c5724937990e203d99d0963a1d5
User <User@DESK-9ZQD132.lexicon.edu.local>
2017-07-13 09:46:09 +0200

    Tja!

--- MicroAirlines/src/Menu.java
+++ MicroAirlines/src/Menu.java
@@ -4,7 +4,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Menu {
-//	Hej Karl!
+
 	static Scanner in = new Scanner(System.in);
 	String val = in.nextLine();
 	
